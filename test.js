c9fc7006a5ba7895ba74a55318494f9e52b01140
869288142 <869288142@qq.com>
2019-08-04 22:15:08 +0800

    本周知识总结

--- test.js
+++ test.js
@@ -1,15 +1,25 @@
 
-let path = "前端"
-const repPath = '.'
-const outputFile = 'SUMMARY.md'
-// 得到需要排除的文件夹
-let include = ["前端","JS","CSS","HTML","浏览器","Vue","Webpack"]
-// let include = []
+// let path = "前端"
+// const repPath = '.'
+// const outputFile = 'SUMMARY.md'
+// // 得到需要排除的文件夹
+// let include = ["前端","JS","CSS","HTML","浏览器","Vue","Webpack"]
+// // let include = []
 
-let g = require("./script")
-let config = {path, repPath, outputFile, include}
-new g(config).run()
-
-
+// let g = require("./script")
+// let config = {path, repPath, outputFile, include}
+// new g(config).run()
 
 
+
+let obj =  {
+  a : "a"
+}
+
+Object.prototype.b = "b" 
+
+Object.defineProperty(obj, "c", {
+  value: "c"
+})
+let {a, b, c} = obj
+console.log(a, b, c);
