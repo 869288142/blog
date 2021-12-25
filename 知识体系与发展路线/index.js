3f7d709808ad35dbe61fe41d269aaf94e053d7e8
869288142 <869288142@qq.com>
2021-12-25 23:33:57 +0800

    feat: 整理学习资料

--- 知识体系与发展路线/index.js
+++ 知识体系与发展路线/index.js
@@ -1,87 +1,34 @@
-const fs = require('fs')
-const loadsh=require('loadsh')
-
-fs.readFile('./stats.json', 'utf8' , (err, data) => {
-  if (err) {
-    console.error(err)
-    return
+//首先我们封装一个时间切片执行器
+function timeSlice(gen) {
+  if (typeof gen !== "function")
+    throw new Error("TypeError: the param expect a generator function");
+  var g = gen();
+  if (!g || typeof g.next !== "function") return;
+  return function next() {
+    var start = performance.now();
+    var res = null;
+    do {
+      res = g.next();
+    } while (res.done !== true && performance.now() - start < 25);
+    if (res.done) return;
+    window.requestIdleCallback(next);
+  };
+}
+//然后把长任务变成generator函数，交由时间切片执行器来控制执行
+const add = function (i) {
+    const listDom = document.querySelector('body')
+  let item = document.createElement("li");
+  item.innerText = `第${i++}条`;
+  listDom.appendChild(item);
+};
+function* gen() {
+  let i = 0;
+  while (i < 100000) {
+    yield add(i);
+    i++;
   }
-
-  console.log(data);
-  const fileObj=JSON.parse(data);//将读取到的文件内容，转换成JSON对象
-  const moduleArr=fileObj.modules;//获取模块数据的数组
-  const moduleObj={};
-  moduleArr.forEach(ele => {    //将数组转换为对象的属性，key值为数组每一项的nameForCondition属性,方便之后查找到该module
-    moduleObj[ele.nameForCondition]=ele;
-  });
-
-  console.log('moduleObj', moduleObj);
- let filename="C:\\Users\\Re\\Desktop\\webpack-demo\\src\\t.js"
-  const tree=[];
-  //从改变的模块向上检索出树形的结构图
-  function createTree(filename,tree){
-    //获取变更的module对象
-    /* const targetmodule=moduleObj[Object.keys(moduleObj).filter(ele=>{
-        return  ele.includes(filename)||ele===filename;
-    })[0]]; */
-
-    const targetmodule=moduleObj[filename];
-    if(!targetmodule){
-        console.log(`未获取到:${filename}模块`);
-        return;
-    };
-    //查看当前的一级中是否已经添加了该module，因为一个module依赖另一个module时，会在该module的reasons中出现多次
-    let isHaveTarget=tree.filter(item=>{
-        return item.name===targetmodule.nameForCondition
-    });
-    if(isHaveTarget&&isHaveTarget.length>0){
-        return;
-    }
-    //将该模块放到该节点中
-    tree.push({
-        name:targetmodule.nameForCondition,
-        children:[]
-    });
-
-    if(targetmodule.reasons&&targetmodule.reasons.length>0){
-        for(let item of targetmodule.reasons){
-            //判断终止条件
-            if(item.type!=='entry' && item.resolvedModuleIdentifier!==tree[tree.length-1].name){
-                createTree(item.resolvedModuleIdentifier,tree[tree.length-1].children);
-            }    
-        }
-    }else{
-        return;
-    }
-  }
-  //以修改的组件为根节点创建一个树形结构数据
-  createTree(filename,tree);
-  //获取哪些组件依赖了该组件
-  console.log('======',JSON.stringify(tree));
-  const pathArr=[];//存放所有路径的数组
-  //打印树结构的所有路径组成的数组
-  function getTreeAllPath(tree){
-
-      function getData(tree,path){
-        tree.forEach(ele => {
-            if(ele.children&&ele.children.length>0){
-                path.push(ele.name);
-                getData(ele.children,path);
-                path.pop();
-            }else{
-                path.push(ele.name);
-                pathArr.push(loadsh.cloneDeep(path));
-                path.pop();
-            }
-        });
-      }
-      getData(tree,[]);
-  }
-
-  getTreeAllPath(tree);
-  //数组的每一项也都是数组，颠倒数组的顺序
-  pathArr.forEach(item=>{
-      item.reverse();
-  })
-  console.log('++++++',JSON.stringify(pathArr));
-})
+}
+//使用时间切片来插入10W条数据
+function bigInsert() {
+  timeSlice(gen)();
+}
